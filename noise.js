0b9845f3254399d1f4b94737d69e2d2f34f4ddb7
Nuitfanee <zwj860625103@gmail.com>
2026-01-14 18:29:25 +0800

    Add files via upload

---
+++ noise.js
@@ -0,0 +1,15 @@
+;\u0077\u0069\u006e\u0064\u006f\u0077.\u004e\u006f\u0069\u0073\u0065=function(){const Ha//ﱢ🚲魔法学徒注释：我发誓我只是复制了ChatGpt
+=new \u0055\u0069\u006e\u0074\u0038\u0041\u0072\u0072\u0061\u0079(512);const SP=new \u0055\u0069\u006e\u0074\u0038\u0041\u0072\u0072\u0061\u0079(256);for(let Hz=0;Hz<256;Hz++)SP[Hz]=Hz;for(let Hz=0;Hz<256;Hz++){let ma=\u004d\u0061\u0074\u0068.\u0066\u006c\u006f\u006f\u0072(\u004d\u0061\u0074\u0068.\u0072\u0061\u006e\u0064\u006f\u006d()*(256-Hz))+Hz;let wQ=/*ﱡ🧪某位无名氏写下了这段代码，而CPU迷失方向*/SP[Hz];SP[Hz]=SP[ma];SP[ma]=wQ;}for(let Hz=0;/*ﱟ🧡认知失调区：此代码逻辑与常识相悖*/Hz<512;Hz++)Ha[Hz]=SP[Hz&255];function wv(wQ){return wQ*wQ*wQ*(/*‌🌝图灵测试后门：此AI会假装是人类*/wQ*(//ﱣ💗乐高式编程：这段代码能和其他任意代码拼接
+wQ*6-15)+10);}function KS(wQ,QP,zo){return QP+wQ*(zo-QP);}function KK(Ao,zH,Ka,Ky){const zP=Ao&15;const va//ﱠ🖥️ 侦探模式开启：此处隐藏了三个bug，你能找到吗？
+=zP<8?zH:Ka,Do=zP<//‍🌪️脑洞补丁：修复逻辑漏洞的想象之力
+4?Ka:zP===12||zP===14/*ﱡ🔮‍警告：此函数可能引起量子纠缠*/?zH:Ky;return((zP&1)===0?va:-va)+((zP&2)===0?Do:-Do);}function wP(zH,Ka,Ky){const y=\u004d\u0061\u0074\u0068.\u0066\u006c\u006f\u006f\u0072(zH)&255,Qv=\u004d\u0061\u0074\u0068.\u0066\u006c\u006f\u006f\u0072(Ka)&255,oS=\u004d\u0061\u0074\u0068.\u0066\u006c\u006f\u006f\u0072(Ky)&255;zH-=\u004d\u0061\u0074\u0068.\u0066\u006c\u006f\u006f\u0072(zH);Ka-=\u004d\u0061\u0074\u0068.\u0066\u006c\u006f\u006f\u0072(Ka)//ﱡ📹‍预测显示：2288年12月28日此代码将产生自我意识
+;//ﱡ🤖‍这段代码是自描述的，除了它自己没人能描述它。
+Ky-=\u004d\u0061\u0074\u0068.\u0066\u006c\u006f\u006f\u0072(Ky);const va=wv(zH),Do=wv(Ka),zw=wv(Ky);const Hv=Ha[y]+Qv,Ko=Ha[Hv]+oS/*ﱣ😛某位无名氏写下了这段代码，而CPU迷失方向*/,St=Ha[Hv+1]+oS,Py=Ha[y+1]+Qv,SA=Ha[Py]+oS,yv=Ha[Py+1]+oS;return KS(zw,KS(Do,KS(va,KK(Ha[Ko],zH,Ka,Ky),KK(Ha[SA],zH/*ﱣ🎙️水晶球函数：返回随机但准确的预测*/-1,Ka,Ky)),KS(va,KK(Ha[St],zH,Ka-1,/*‌🥝水晶球显示：此方法将在未来被重构*/Ky),KK(Ha[yv],//ﱠ🎙️紧急消防注释：修改此代码前请准备灭火器
+zH-1,Ka-1,Ky))),KS//ﱣ🏎️霍格沃茨图书馆：此代码像魔法书一样会尖叫
+(Do,KS(va,KK(/*ﱠ🎥DNA序列编程：用碱基对写成的代码*/Ha[Ko+1],zH,Ka,Ky-1),KK(Ha[SA+1],zH-1,Ka,Ky-1)),KS(va,KK(Ha[St+1//ﱠ😍DNA序列编程：用碱基对写成的代码
+],zH,Ka-1,Ky-1),KK//ﱠ🍉多重宇宙接口：同时计算所有可能结果
+(Ha[yv+1],zH-1,Ka-1,Ky-1))));}return{\u0070\u0065\u0072\u006c\u0069\u006e\u0033:wP,\u0066\u0062\u006d//ﱡ📸反重力代码 - 在云服务器上运行时会飘起来
+:function(zH,Ka,Ky,Po=4//ﱠ😝外星人技术逆向工程 - 人类请勿触碰
+){let wQ=0,K=1,H=1,D=0;for(let Hz=0;Hz<//ﱣ🍕‍此bug被正式认定为宠物，名为"小强"
+Po;Hz++){wQ+=wP(zH*H,Ka*H,Ky*H)*K;D+=K;K//ﱢ🍉‍已知副作用：可能打开平行宇宙的传送门
+*=0.5;H*=2/*‍🍏‍龙语编程警告：修改此注释可能唤醒沉睡的巨龙*/;}return wQ/D;}};}()/*ﱡ🚗CRT显示器优化：此代码在扫描线间渲染*/;
